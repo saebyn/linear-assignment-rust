b6e11b3ebf0c2773c0c172c19fe9795c6dd1b2c4
John Weaver <john@saebyn.info>
2019-12-06 09:45:59 -0800

    Make benches run on stable rust.

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,47 +1,50 @@
 // Copyright (c) 2015 John Weaver and contributors.
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#![feature(test)]
+extern crate criterion;
 
-extern crate nalgebra as na;
-extern crate test;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+extern crate rand;
+use rand::Rng;
+
 extern crate linear_assignment;
+extern crate nalgebra as na;
 
 use std::collections::HashSet;
-use na::Transpose;
-use test::Bencher;
 
 use linear_assignment::*;
-
 
 trait LinearAssignmentProblem {
     fn munkres(&self) -> HashSet<Edge>;
 }
 
-
-impl LinearAssignmentProblem for na::DMat<u32> {
+impl LinearAssignmentProblem for na::DMatrix<u32> {
     fn munkres(&self) -> HashSet<Edge> {
-        let mut matrix = &mut na::DMat::<u32>::new_zeros(self.nrows(), self.ncols());
+        let mut matrix = &mut na::DMatrix::<u32>::zeros(self.nrows(), self.ncols());
         matrix.clone_from(self);
         let transposed = self.nrows() > self.ncols();
 
         if transposed {
             matrix.transpose_mut();
         }
-        let size = MatrixSize { rows: matrix.nrows(), columns: matrix.ncols() };
+        let size = MatrixSize {
+            rows: matrix.nrows(),
+            columns: matrix.ncols(),
+        };
         assert!(size.columns >= size.rows);
-        let edges = solver::<na::DMat<u32>>(&mut matrix, &size);
+        let edges = solver::<na::DMatrix<u32>>(&mut matrix, &size);
         if transposed {
             edges.iter().map(|&(v, u)| (u, v)).collect()
         } else {
@@ -50,9 +53,15 @@ impl LinearAssignmentProblem for na::DMat<u32> {
     }
 }
 
-
-#[bench]
-fn bench_square(b: &mut Bencher) {
-    let matrix = na::DMat::<u32>::from_fn(100, 100, |x, y| x as u32 * y as u32);
-    b.iter(|| matrix.munkres());
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut rng = rand::thread_rng();
+    c.bench_function("square matrix", |b| {
+        b.iter(|| {
+            let matrix = black_box(na::DMatrix::<u32>::from_fn(100, 100, |_x, _y| rng.gen()));
+            matrix.munkres()
+        })
+    });
 }
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
